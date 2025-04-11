e5d41b2efec02dda91850a84e0a0588ed65d66a5
Kolya <rainyrse@gmail.com>
2025-04-11 23:01:30 +0300

    Добавление title + favicon; Улучшение ui; Работа с отображением модальных окон.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,34 @@ import Profile from '../src/pages/profile/profile';
 import Login from '../src/pages/auth/login';
 import Register from '../src/pages/auth/register';
 import Header from '../src/components/layout/Header';
+import { ThemeProvider, GlobalStyles } from '@mui/material';
+import theme from "./styles/theme"
+import './styles/global.css'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles
+        styles={(theme) => ({
+          ':root': {
+            '--primary-main': theme.palette.primary.main,
+            '--primary-contrastText': theme.palette.primary.contrastText,
+            '--secondary-main': theme.palette.secondary.main,
+            '--secondary-light': theme.palette.secondary.light,
+            '--background-default': theme.palette.background.default,
+            '--background-paper': theme.palette.background.paper,
+          }
+        })}
+      />
+      <BrowserRouter>
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
