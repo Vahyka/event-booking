ff7fe0740cf418db21bc87f0d860efa57ab16e47
Vadim <gwwin@bk.ru>
2025-04-17 01:54:58 +0300

    Фиксанул docker-compose. Все собирается.

--- backend/api/src/routes/eventsRoutes.ts
+++ backend/api/src/routes/eventsRoutes.ts
@@ -1,13 +1,14 @@
 import express from 'express';
-// import db from '../db.ts'
 
 const router = express.Router();
 
 // Show up the list of events
 router.get('/', (req, res) => {
-    res.status(202).send("get events!");
+    res.status(202).send("Fetching events!");
 });
 // Request to buy a ticket
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+    res.send('Saving events!');
+});
 
 export default router;
